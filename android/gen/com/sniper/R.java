1970c87dc8be8e2282bc9c4ce5f471bbeab92390
Chelsey Denton <cdenton@wisc.edu>
2013-11-06 17:38:11 -0600

    Toast when picture taken

--- android/gen/com/sniper/R.java
+++ android/gen/com/sniper/R.java
@@ -39,18 +39,19 @@ public final class R {
         public static final int target_image=0x7f02000c;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000b;
-        public static final int armory_button=0x7f090007;
-        public static final int camera_preview=0x7f090002;
-        public static final int game_button=0x7f090006;
-        public static final int games_list=0x7f090001;
-        public static final int home_button=0x7f090008;
-        public static final int leaders_button=0x7f090009;
-        public static final int name_button=0x7f090003;
-        public static final int settings_button=0x7f09000a;
-        public static final int target_image=0x7f090004;
+        public static final int action_settings=0x7f09000c;
+        public static final int armory_button=0x7f090008;
+        public static final int camera_preview=0x7f090003;
+        public static final int game_button=0x7f090007;
+        public static final int games_list=0x7f090002;
+        public static final int home_button=0x7f090009;
+        public static final int leaders_button=0x7f09000a;
+        public static final int mygamesheading=0x7f090001;
+        public static final int name_button=0x7f090004;
+        public static final int settings_button=0x7f09000b;
+        public static final int target_image=0x7f090005;
         public static final int toolbar_fragment=0x7f090000;
-        public static final int weapon_button=0x7f090005;
+        public static final int weapon_button=0x7f090006;
     }
     public static final class layout {
         public static final int activity_armory_home=0x7f030000;
--- android/src/com/sniper/Toolbar.java
+++ android/src/com/sniper/Toolbar.java
@@ -3,14 +3,17 @@ package com.sniper;
 import com.sniper.core.Camera;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Toolbar extends Fragment{
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,6 +44,12 @@ public class Toolbar extends Fragment{
 	            	
 	            	if(MainActivity.class.isInstance(activity)){
 	            		Camera.TakePicture();
+	            		
+	            		CharSequence text = "You just tried to shoot Chelsey";
+	            		int duration = Toast.LENGTH_SHORT;
+	            		Toast toast = Toast.makeText(Camera.context, text, duration);
+	            		toast.setGravity(Gravity.CENTER, 0, 0);
+	            		toast.show();
 	            	}
 	            	else{
 		            	Intent intent = new Intent(activity, MainActivity.class);
