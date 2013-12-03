0e5eb15af9aa787b065a88b6be2df7898a452e56
Chelsey Denton <cdenton@wisc.edu>
2013-12-03 15:43:37 -0600

    Cache downloaded images in hashmap

--- android/src/com/sniper/utility/LoadUserImage.java
+++ android/src/com/sniper/utility/LoadUserImage.java
@@ -3,6 +3,9 @@ package com.sniper.utility;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
@@ -16,7 +19,15 @@ import com.parse.ParseUser;
 import com.sniper.R;
 
 public class LoadUserImage {
+	private static HashMap<String, Bitmap> map = new HashMap<String, Bitmap>();
+	
 	public static void GetImage(ParseUser user, Activity activity){
+		Bitmap b = map.get(user.getObjectId());
+		if(b != null){
+			activity.runOnUiThread(new LoadUserImage().new UpdateImage(b, activity));
+			return;
+		}
+		
 		URL url = null;
 		try {
 			url = new URL("https://s3.amazonaws.com/sniper_profilepictures/" 
@@ -24,7 +35,7 @@ public class LoadUserImage {
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}		
-		GetImageRequest request = new GetImageRequest(activity);
+		GetImageRequest request = new GetImageRequest(activity, user);
 		request.execute(url);
 	}
 	
@@ -49,10 +60,13 @@ public class LoadUserImage {
 	    /** application context. */
         private Context context;
 	    
-		public GetImageRequest(Activity activity){
+        private String userId;
+        
+		public GetImageRequest(Activity activity, ParseUser user){
 			this.activity = activity;
 			context = activity;
 			dialog = new ProgressDialog(context);
+			userId = user.getObjectId();
 		}
 		
 		 protected void onPreExecute() {
@@ -72,6 +86,7 @@ public class LoadUserImage {
 			
 			try {
 				Bitmap image = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+				map.put(userId, image);
 				activity.runOnUiThread(new LoadUserImage().new UpdateImage(image, activity));
 			} catch (IOException e) {
 				e.printStackTrace();
